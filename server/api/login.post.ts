a0762a4b4ab58b1f87498674ba6d141672621358
kouacc <maxrur57@gmail.com>
2024-10-17 11:29:22 +0200

    fix: fix imports

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,37 +1,36 @@
-import { db } from '../utils/orm';
+import { db } from "../utils/orm";
 import { userTable } from "~/database/schema";
-import { eq } from '../utils/orm';
-import { generateJWT } from '../utils/jwt';
-import crypto from 'crypto'
+import { eq } from "../utils/orm";
+import { generateJWT } from "../../utils/jwt";
+import crypto from "crypto";
 
 export default defineEventHandler(async (event) => {
-    const formData = await readFormData(event)
-    const username = formData.get("username") as string
-    const password = formData.get("password") as string
+  const formData = await readFormData(event);
+  const username = formData.get("username") as string;
+  const password = formData.get("password") as string;
 
-    const user = await db.query.userTable.findFirst({
-        where: eq(userTable.username, username)
-    })
+  const user = await db.query.userTable.findFirst({
+    where: eq(userTable.username, username),
+  });
 
-    if (!user || !user.salt) {
-        return {
-            "status": 401,
-            "body": "Nom d'utilisateur invalide."
-        }
-    }
+  if (!user || !user.salt) {
+    return {
+      status: 401,
+      body: "Nom d'utilisateur invalide.",
+    };
+  }
 
-    const hash = crypto.pbkdf2Sync(password, user.salt, 310000, 32, 'sha256')
+  const hash = crypto.pbkdf2Sync(password, user.salt, 310000, 32, "sha256");
 
-    if (Buffer.compare(hash, Buffer.from(user.password_hash)) === 0) {
-        const token = generateJWT(user.id, user.username, user.name)
-        const cookie = setCookie(event, 'token', token)
-        // return cookie
-        return cookie
-    } else {
-        return {
-            "status": 401,
-            "body": "Mot de passe invalide."
-        }
-    }
-    
+  if (Buffer.compare(hash, Buffer.from(user.password_hash)) === 0) {
+    const token = generateJWT(user.id, user.username, user.name);
+    const cookie = setCookie(event, "token", token);
+    // return cookie
+    return cookie;
+  } else {
+    return {
+      status: 401,
+      body: "Mot de passe invalide.",
+    };
+  }
 });
---
+++ server/api/protected/decode.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler(async (event) => {
+  // read the authorization header
+  const authorization = event.headers.get("Authorization");
+  if (!authorization) {
+    return {
+      status: 401,
+      body: "Unauthorized",
+    };
+  }
+
+  return verifyJWT(authorization)
+});
