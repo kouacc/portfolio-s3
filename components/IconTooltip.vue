e45eeb90b65cd51c7258c874738c5b7f62550893
kouacc <maxrur57@gmail.com>
2024-10-17 16:19:16 +0200

    feat: added icon with tooltip component

---
+++ components/IconTooltip.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+    name: string
+    size?: string
+    iconClass?: string
+    parentClass?: string
+    position?: 'left' | 'right' | 'top' | 'bottom'
+    }>(), {
+    size: '24',
+    class: '',
+    position: 'top'
+})
+</script>
+
+<template>
+    <div class="w-fit relative flex flex-col items-center group" :class="props.parentClass">
+        <Icon :name="props.name" :size="props.size" :class="props.iconClass" />
+        <div class="absolute bottom-[calc(1rem+1ch)] flex flex-col items-center hidden mb-5 group-hover:flex group-[]:transition-all">
+            <div class="transition-all px-3 py-1.5 rounded-xl bg-primary dark:bg-primary_dark border border-secondary dark:border-secondary_dark">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
