0d2cf9137c45ba8e623ccc2275189c4d43755b74
kouacc <maxrur57@gmail.com>
2024-10-17 09:13:22 +0200

    fix: fix JWT imports

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -1,18 +1,18 @@
-import * as jwt from "jsonwebtoken";
+import jsonwebtoken from "jsonwebtoken";
 
 interface JWT_Structure {
   id: Number;
   username: String;
 }
 
-export function generateJWT(id: number, username: string): string {
-  return jwt.sign(
-    { id: id, username: username },
-    process.env.JWT_TOKEN as string
+export function generateJWT(id: number, username: string, surname: string): string {
+  return jsonwebtoken.sign(
+    { id: id, username: username, name: surname },
+    process.env.JWT_SECRET as string
   );
 }
 
 export function verifyJWT(token: string) {
-  var decoded = jwt.verify(token, process.env.JWT_TOKEN as string);
+  var decoded = jsonwebtoken.verify(token, process.env.JWT_TOKEN as string);
   return decoded;
 }
