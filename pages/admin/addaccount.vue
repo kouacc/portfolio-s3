a8042916d976a791614eb7add415e05cda06e5fe
Maxence Lallemand <maxence.lallemand@edu.univ-fcomte.fr>
2024-10-18 22:47:02 +0200

    chore: handle adding new account

--- pages/admin/addaccount.vue
+++ pages/admin/addaccount.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
-  middleware: 'auth'
+  /* middleware: 'auth' */
 })
 
 useHead({
@@ -10,14 +10,19 @@ useHead({
   title: 'Gestion - Maxence Lallemand'
 })
 
-const submitForm = async () => {
+const submitForm = async (event: Event) => {
+  event.preventDefault()
+  const { data } = await useFetch('/api/createuser', {
+    method: 'POST',
+    body: new FormData(event.target as HTMLFormElement)
+  })
 }
 </script>
 
 <template>
   <div class="container py-24">
     <h1>Ajouter un compte</h1>
-    <form @submit="submitForm" class="flex flex-col">
+    <form @submit="submitForm" method="post" class="flex flex-col">
         <label class="flex flex-col gap-2.5">
             <span class="text-fill dark:text-fill_dark">Nom d'utilisateur</span>
             <input class="rounded-xl bg-primary dark:bg-primary_dark border-secondary dark:border-secondary_dark" type="text" name="username">
--- server/api/createuser.ts
+++ server/api/createuser.ts
@@ -4,8 +4,12 @@ import { eq } from "../utils/orm";
 import crypto from 'crypto'
 
 export default defineEventHandler(async (event) => {
+    const formData = await readFormData(event);
+    const username = formData.get("username") as string;
+    const name = formData.get("name") as string;
+    const password = formData.get("password") as string;
     var salt = crypto.randomBytes(16)
-    const hashed_password = crypto.pbkdf2Sync(process.env.ADMIN_PASSWORD as string, salt, 310000, 32, 'sha256')
+    const hashed_password = crypto.pbkdf2Sync(password as string, salt, 310000, 32, 'sha256')
 
-    await db.insert(userTable).values({ username: "kouacc", name: 'Maxence', password_hash: hashed_password, salt: salt, })
+    await db.insert(userTable).values({ username: username, name: name, password_hash: hashed_password, salt: salt, })
 });
