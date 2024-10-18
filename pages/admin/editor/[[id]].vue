ce9d24857aa3c2b9b0dfa91c7962aaa3a0466611
Maxence Lallemand <maxence.lallemand@edu.univ-fcomte.fr>
2024-10-18 23:32:09 +0200

    feat: handle images upload

--- pages/admin/editor/[[id]].vue
+++ pages/admin/editor/[[id]].vue
@@ -28,6 +28,9 @@ const project = reactive({
   tools: []
 });
 
+const project_imgs = ref<string[]>([]);
+const fileInput = ref<HTMLInputElement | null>(null);
+
 if (route.params.id) {
   const { data } = await useFetch<Project>(`/api/projects/${route.params.id}`);
   if (data) {
@@ -72,6 +75,24 @@ function handleToolClick(slug: string) {
   }
 }
 
+const onFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.files) {
+    const files = Array.from(target.files);
+    project_imgs.value = [];
+
+    files.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        if (e.target?.result) {
+          project_imgs.value.push(e.target.result as string);
+        }
+      };
+      reader.readAsDataURL(file);
+    });
+  }
+};
+
 const isSaved = ref<boolean>(true)
 const update = debounce((e) => {
   project.content = e.target.value
@@ -86,18 +107,19 @@ const output = computed(() => marked(project.content))
     </div>
     <nav class="mb-16 fixed bottom-0 left-1/2 -translate-x-1/2 flex flex-row items-center gap-5">
         <ul class="flex items-center gap-3 bg-primary dark:bg-primary_dark px-3 py-2 rounded-xl border border-secondary dark:border-secondary_dark">
-            <li><button @click="settingsOverlay = true" class="inline-flex items-center"><IconTooltip name="fa6-solid:gear" size="24" class="mx-2 my-1.5"><span>Paramètres du projet</span></IconTooltip></button></li>
-            <li><NuxtLink class="inline-flex items-center cursor-pointer"><IconTooltip name="fa6-solid:expand" size="24" class="mx-2 my-1.5"><span>Aperçu</span></IconTooltip></NuxtLink></li>
+            <li><button @click="settingsOverlay = true" class="inline-flex items-center transition-all rounded-lg hover:bg-white/10"><IconTooltip name="fa6-solid:gear" size="24" class="mx-2 my-1.5"><span>Paramètres du projet</span></IconTooltip></button></li>
+            <li><NuxtLink class="inline-flex items-center cursor-pointer transition-all rounded-lg hover:bg-white/10"><IconTooltip name="fa6-solid:expand" size="24" class="mx-2 my-1.5"><span>Aperçu</span></IconTooltip></NuxtLink></li>
             <li><ActionButton variant="primary" href="/admin/editor"><span class="text-white">{{ !route.params.id ? 'Créer le projet' : 'Modifier le projet' }}</span></ActionButton></li>
         </ul>
         <Icon v-if="isSaved" name="fa6-solid:circle-check" size="24" class="text-green-500"/>
         <Icon v-else name="lucide:hourglass" size="24" class="text-red-500"/>
     </nav>
+    <Transition>
     <div v-show="settingsOverlay" class="w-screen h-screen absolute z-50 top-0 bg-black/60 transition-all">
       <div class="bg-primary dark:bg-primary_dark absolute bottom-0 w-1/2 h-[calc(100vh-6rem)] p-8 rounded-r-xl space-y-6">
         <section class="flex justify-between">
           <h2>Paramètres</h2>
-          <button @click="settingsOverlay = false"><Icon name="lucide:x" size="32" /></button>
+          <button @click="settingsOverlay = false" class="flex p-2 rounded-xl transition-all hover:bg-white/10"><Icon name="lucide:x" size="32" /></button>
         </section>
         <div class="space-y-5">
           <label class="flex flex-col gap-5">
@@ -115,13 +137,19 @@ const output = computed(() => marked(project.content))
             </label>
             <label class="flex flex-1 flex-col gap-5">
               <span>Outils</span>
-              <button @click="tools.isModalOpen = true" class="bg-secondary dark:bg-secondary_dark text-fill dark:text-fill_dark rounded-xl px-5 py-2">Sélectionner</button>
+              <button @click="tools.isModalOpen = true" class="bg-secondary dark:bg-secondary_dark text-fill dark:text-fill_dark rounded-xl px-5 py-2 transition-all hover:bg-white/25">Sélectionner</button>
             </label>
           </div>
-          <label class="flex flex-col gap-5">
-            <span>Images et médias</span>
-            <input class="file:rounded-xl file:px-5 file:py-2 file:border-none file:bg-secondary dark:file:bg-secondary_dark file:text-fill dark:file:text-fill_dark font-geist" type="file" accept="image/jpeg, image/webp, image/png, image/gif" multiple>
-          </label>
+          <div class="flex items-end">
+            <label class="flex flex-col gap-5">
+              <span>Images et médias</span>
+              <input ref="fileInput" class="file:rounded-xl file:px-5 file:py-2 file:border-none file:bg-secondary dark:file:bg-secondary_dark file:text-fill dark:file:text-fill_dark file:hover:bg-white/25 font-geist file:transition-all" type="file" accept="image/jpeg, image/webp, image/png, image/gif" multiple @change="onFileChange">
+            </label>
+            <ActionButton variant="secondary" @click="project_imgs = [], fileInput.value = ''"><Icon name="lucide:trash-2" class="text-white" /><span class="text-white">Tout supprimer</span></ActionButton>
+          </div>
+          <ul>
+            <li v-for="img in project_imgs" :key="img" class="hover:bg-red-500/20"><img :src="img" class="h-52"></li>
+          </ul>
         </div>
       </div>
       <div v-show="tools.isModalOpen" class="absolute w-2/5 h-[calc(100vh-6rem)] right-0 bottom-0 rounded-tl-xl bg-primary dark:bg-primary_dark p-8 space-y-5">
@@ -158,4 +186,5 @@ const output = computed(() => marked(project.content))
         </div>
       </div>
     </div>
+    </Transition>
 </template>
