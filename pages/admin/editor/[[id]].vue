fc67fdc4041beec65529e7a5715ad6b7aba725b7
kouacc <maxrur57@gmail.com>
2024-10-18 14:11:09 +0200

    feat: change icons to icon tooltip component

--- pages/admin/editor/[[id]].vue
+++ pages/admin/editor/[[id]].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { debounce } from 'lodash-es'
 import { marked } from 'marked'
+import { useStorage } from '@vueuse/core';
 const route = useRoute('/admin/editor/:id')
 
 useHead({
@@ -16,11 +17,19 @@ definePageMeta({
 })
 
 const isSaved = ref<boolean>(true)
-const input = ref('# Nouveau projet')
+const input = ref<string>('# Nouveau projet')
 const update = debounce((e) => {
   input.value = e.target.value
 }, 100)
 const output = computed(() => marked(input.value))
+
+watch(input, () => {
+  isSaved.value = false
+  useStorage('editor', input.value)
+  setTimeout(() => {
+    isSaved.value = true
+  }, 1000)
+})
 </script>
 
 <template>
@@ -30,8 +39,8 @@ const output = computed(() => marked(input.value))
 
     <nav class="mb-16 fixed bottom-0 left-1/2 -translate-x-1/2 flex flex-row items-center gap-5">
         <ul class="flex items-center gap-3 bg-primary dark:bg-primary_dark px-3 py-2 rounded-xl border border-secondary dark:border-secondary_dark">
-            <li><button class="inline-flex items-center"><Icon name="fa6-solid:gear" size="24" class="mx-2 my-1.5"/></button></li>
-            <li><NuxtLink class="inline-flex items-center"><Icon name="fa6-solid:expand" size="24" class="mx-2 my-1.5"/></NuxtLink></li>
+            <li><button class="inline-flex items-center"><IconTooltip name="fa6-solid:gear" size="24" class="mx-2 my-1.5"><span>Paramètres du projet</span></IconTooltip></button></li>
+            <li><NuxtLink class="inline-flex items-center cursor-pointer"><IconTooltip name="fa6-solid:expand" size="24" class="mx-2 my-1.5"><span>Aperçu</span></IconTooltip></NuxtLink></li>
             <li><ActionButton variant="primary" href="/admin/editor"><span class="text-white">Créer le projet</span></ActionButton></li>
         </ul>
         <Icon v-if="isSaved" name="fa6-solid:circle-check" size="24" class="text-green-500"/>
