8d5587343cb22399b6018257616dee63e3438c19
kouacc <maxrur57@gmail.com>
2024-10-18 14:10:27 +0200

    chore: change jsonwebtoken package to jose

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,18 +1,31 @@
-import jsonwebtoken from "jsonwebtoken";
+import * as jose from "jose";
 
 interface JWT_Structure {
   id: Number;
   username: String;
 }
 
-export function generateJWT(id: number, username: string, surname: string): string {
-  return jsonwebtoken.sign(
-    { id: id, username: username, name: surname },
-    process.env.JWT_SECRET as string
-  );
+const signature = new TextEncoder().encode(process.env.JWT_SECRET as string);
+const alg = "HS256";
+
+export async function generateJWT(
+  id: number,
+  username: string,
+  surname: string
+) {
+    const jwt = await new jose.SignJWT({
+    id: id,
+    username: username,
+    name: surname,
+  })
+    .setIssuedAt()
+    .setExpirationTime("2h")
+    .setProtectedHeader({ alg })
+    .sign(signature);
+  return jwt;
 }
 
-export function verifyJWT(token: string) {
-  var decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET as string);
+export async function verifyJWT(token: string) {
+  var decoded = await jose.jwtVerify(token, signature);
   return decoded;
 }
