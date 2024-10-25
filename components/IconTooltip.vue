475d2dcb4d1e4e1c3bd493d1af5295e3e3ee19c3
kouacc <maxrur57@gmail.com>
2024-10-25 13:02:44 +0200

    fix: change group name on hover

--- components/IconTooltip.vue
+++ components/IconTooltip.vue
@@ -13,9 +13,9 @@ const props = withDefaults(defineProps<{
 </script>
 
 <template>
-    <div class="w-fit relative flex flex-col items-center group" :class="props.parentClass">
+    <div class="w-fit relative flex flex-col items-center group/icon" :class="props.parentClass">
         <Icon :name="props.name" :size="props.size" :class="props.iconClass" />
-        <div class="absolute bottom-[calc(1rem+1ch)] flex flex-col items-center hidden mb-5 group-hover:flex group-[]:transition-all">
+        <div class="absolute bottom-[calc(1rem+1ch)] flex flex-col items-center hidden mb-5 group-hover/icon:flex group-[]/icon:transition-all">
             <div class="transition-all px-3 py-1.5 rounded-xl bg-primary dark:bg-primary_dark border border-secondary dark:border-secondary_dark">
                 <slot />
             </div>
