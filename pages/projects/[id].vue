a5825218465df7f59c6ee1e3d11b86048d1969e0
kouacc <maxrur57@gmail.com>
2024-10-26 18:23:48 +0200

    feat: started project ID page

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { type Project } from '~/database/schema';
+import { marked } from 'marked';
+
 const $route = useRoute()
 useHead({
   bodyAttrs: {
@@ -6,12 +9,22 @@ useHead({
   }
 })
 
-const { data } = useFetch('/api/projects/:id', { params: { id: $route.params.id } })
+const { data } = useFetch<Project>(`/api/projects/${$route.params.id}`)
+
+const output = computed(() => data?.value ? marked(data.value.content) : '')
 </script>
 
 <template>
-    <div class="gradient-bg py-24 container">
-        <h1 class="text-7xl megazoid-text-h1 text-white" style="-webkit-text-stroke-width: 5px;">Projets</h1>
-        <h2 v-if="data">Projet {{ data.title }}</h2>
+    <div class="py-24 container space-y-4">
+      <NuxtLink to="/projects" class="inline-flex items-center gap-3 font-geistmono px-4 py-2.5 bg-primary dark:bg-primary_dark border border-secondary dark:border-secondary_dark rounded-xl"><Icon name="lucide:chevron-left" size="24"/>Retour</NuxtLink>
+      <section class="flex flex-col sm:flex-row justify-between sm:items-center">
+        <h1 v-if="data">{{ data.title }}</h1>
+        <div class="font-geistmono flex flex-row sm:flex-col">
+          <span>Ajouté le {{ data?.created_at }}</span>
+          <span>Modifié le {{ data?.modified_at }}</span>
+        </div>
+      </section>
+      <div v-html="output"></div>
+      <div><Icon v-for="tool in data?.tools" :key="tool" :name="`simple-icons:${tool}`" size="24"/></div>
     </div>
 </template>
