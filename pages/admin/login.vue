927e9bfbcbfc132b6a0221d5352c072ef31194e3
kouacc <maxrur57@gmail.com>
2024-10-27 18:22:54 +0100

    feat: add styles to login page

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -7,13 +7,19 @@ useHead({
   title: 'Connexion - Maxence Lallemand'
 })
 
+const router = useRouter()
+
 const submitForm = async (event: Event) => {
     event.preventDefault()
     const { data } = await useFetch('/api/login', {
         method: 'POST',
         body: new FormData(event.target as HTMLFormElement)
-
     })
+    if (data.value?.status === 401) {
+        alert('Identifiants incorrects')
+    } else {
+        router.push('/admin')
+    }
 }
 
 const form = ref({
@@ -23,12 +29,20 @@ const form = ref({
 </script>
 
 <template>
-    <div class="container py-24 h-screen">
-        <h1>Gestion du site</h1>
-        <form @submit="submitForm" class="flex flex-col gap-10">
-            <input type="text" name="username" id="" required v-model="form.username">
-            <input type="password" name="password" id="" required v-model="form.password">
-            <input type="submit" value="Connexion">
+    <div class="container py-24 h-screen space-y-8">
+        <h1 class="megazoid-text-h1 w-fit text-white" data-text="Backoffice">Backoffice</h1>
+        <form @submit="submitForm" class="flex flex-col gap-10 relative mx-48">
+            <div class="col-start-3 col-span-8 flex flex-col gap-7 dot-grid dark:dot-grid-dark px-24 py-20">
+                <label class="flex flex-col gap-2.5">
+                    Nom d'utilisateur
+                    <input type="text" name="username" id="" required v-model="form.username">
+                </label>
+                <label class="flex flex-col gap-2.5">
+                    Mot de passe
+                    <input type="password" name="password" id="" required v-model="form.password">
+                </label>
+                <input class="primary-btn-submit place-self-center" type="submit" value="Connexion">
+            </div>
         </form>
         
     </div>
