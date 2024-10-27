32c45dc9d07c561460e4522a97d8f0b2b2daaa63
kouacc <maxrur57@gmail.com>
2024-10-27 18:24:37 +0100

    feat: add projects page styles/layout

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -9,7 +9,7 @@ const { data } = await useFetch("/api/projects/fetchprojects");
 </script>
 
 <template>
-  <div class="py-24 container fill-text space-y-12">
+  <div class="pt-24 pb-48 container fill-text space-y-12">
     <h1
       class="text-7xl megazoid-text-h1 text-white"
       data-text="Projets"
@@ -22,12 +22,18 @@ const { data } = await useFetch("/api/projects/fetchprojects");
       <ProjectCard class="col-start-3" href="/projects/1" projectName="Test" projectDate="01/01/2024" projectImg="/Frame 1.jpg"/>
       <ProjectCard class="col-start-3" href="/projects/1" projectName="Test" projectDate="01/01/2024" projectImg="/Frame 1.jpg"/>
     </div>
-    <section class="space-y-14">
-      <h2>Tous mes projets</h2>
+    <div class="space-y-14">
+      <section class="flex justify-between items-center">
+        <h2>Tous mes projets</h2>
+        <div class="flex gap-2">
+          <button class="pagination-btn"><Icon name="lucide:chevron-left" size="24" /></button>
+          <button class="pagination-btn"><Icon name="lucide:chevron-right" size="24" /></button>
+        </div>
+      </section>
       <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        <ProjectCard v-for="projet in data" :key="projet.id" href="/projects/1" :projectName="projet.title" :projectDate="projet.year" projectImg="/Frame 1.jpg" />
+        <ProjectCard v-for="projet in data" :key="projet.id" :href="`/projects/${projet.id}`" :projectName="projet.title" :projectDate="projet.year" projectImg="/Frame 1.jpg" />
       </div>
-    </section>
+    </div>
     
   </div>
 </template>
