944b489dd7243a91243cea7215a868b3cfc58e67
kouacc <maxrur57@gmail.com>
2024-10-27 14:38:29 +0100

    fix: make card more responsive

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -16,9 +16,9 @@ const props = withDefaults(
 </script>
 
 <template>
-    <NuxtLink :href="props.href" class="flex border-button w-fit h-48">
-        <div class="relative overflow-hidden rounded-xl shadow-lg border-2 secondary-border group transition-all delay-100">
-            <img class="w-full h-56 object-cover" :src="projectImg" alt="project image" />
+    <NuxtLink :href="props.href" class="flex border-button w-fit">
+        <div class="relative overflow-hidden rounded-xl shadow-lg border-2 secondary-border group transition-all delay-100 w-full">
+            <img class="w-full h-full object-cover" :src="projectImg" alt="project image" />
             <div class="opacity-0 h-full absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black to-black/0 bg-opaciy-40 place-content-end group-has-[:hover]:opacity-100 transition-opacity delay-100">
                 <section>
                     <h3 class="text-lg font-semibold text-white">{{ projectName }}</h3>
