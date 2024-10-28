778f11b0fe1cc4c9e509d95834a0545a036e0fa1
kouacc <maxrur57@gmail.com>
2024-10-28 17:46:37 +0100

    feat: add layout for project page

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { type Project } from "~/database/schema";
 import { marked } from "marked";
+import icons from '~/assets/icons.json';
 
 const $route = useRoute();
 useHead({
@@ -12,6 +13,9 @@ useHead({
 const { data } = useFetch<Project>(`/api/projects/${$route.params.id}`);
 
 const output = computed(() => (data?.value ? marked(data.value.content) : ""));
+
+const created_date = data?.value ? new Date(data?.value.created_at).toLocaleString('fr-FR', { timeZone: 'UTC', dateStyle: "short" }) : null;
+const modified_date = data?.value ? new Date(data.value.modified_at).toLocaleString('fr-FR', { timeZone: 'UTC', dateStyle: "short" }) : null;
 </script>
 
 <template>
@@ -23,19 +27,36 @@ const output = computed(() => (data?.value ? marked(data.value.content) : ""));
     >
     <section class="flex flex-col sm:flex-row justify-between sm:items-center">
       <h1 v-if="data">{{ data.title }}</h1>
-      <div class="font-geistmono flex flex-row sm:flex-col">
-        <span>Ajouté le {{ data?.created_at }}</span>
-        <span>Modifié le {{ data?.modified_at }}</span>
+      <div class="hidden font-geistmono sm:flex sm:flex-col">
+        <span>Ajouté le {{ created_date }}</span>
+        <span>Modifié le {{ modified_date }}</span>
       </div>
+      <span class="sm:hidden font-geistmono text-xs">Ajouté le {{ created_date }} • Modifié le {{ modified_date }}</span>
     </section>
-    <div v-html="output"></div>
-    <div>
-      <Icon
-        v-for="tool in data?.tools"
-        :key="tool"
-        :name="`simple-icons:${tool}`"
-        size="24"
-      />
+    <div class="content-grid">
+      <div class="col-start-1 col-span-5 space-y-8">
+        <img class="rounded-xl" src="/Frame 1.jpg" alt="">
+        <div class=" rounded-xl px-12 py-11 space-y-12 dot-grid dark:dot-grid-dark bg-background dark:bg-background_dark">
+          <div class="font-geistmono flex flex-col gap-3">
+            <span>Année de réalisation</span>
+            <span>{{ data?.year }}</span>
+          </div>
+          <div class="font-geistmono flex flex-col gap-3">
+            <span>Statut</span>
+            <span>{{ data?.status }}</span>
+          </div>
+          <div class="font-geistmono flex flex-col gap-3">
+            <span>Outils</span>
+            <ul class="flex gap-3">
+              <li v-for="tool in data?.tools" :key="tool">
+                <IconTooltip :name="`simple-icons:${tool}`" size="36">{{ icons.icons.find(d => d.slug === tool)?.title }}</IconTooltip>
+              </li>
+            </ul>
+          </div>
+          <NuxtLink v-if="data?.repository_link" :href="data?.repository_link" class="text-underline">Lien du repository</NuxtLink>
+        </div>
+      </div>
+      <div class="col-start-6 col-span-full prose dark:prose-invert" v-html="output"></div>
     </div>
   </div>
 </template>
