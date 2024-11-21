85f68e6b8b61ceae8a06b3b15c639a0732d071c2
kouacc <maxrur57@gmail.com>
2024-11-21 09:59:36 +0100

    chore: add date formatting for card

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -16,7 +16,7 @@ const props = defineProps<{
             <div class="opacity-0 h-full absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black to-black/0 bg-opaciy-40 place-content-end group-has-[:hover]:opacity-100 transition-opacity delay-100">
                 <section>
                     <h3 class="text-lg font-semibold text-white">{{ projectName }}</h3>
-                    <p class="font-geistmono text-sm text-white">{{ projectDate }}</p>
+                    <p class="font-geistmono text-sm text-white">{{ new Date(projectDate).toLocaleDateString('fr') }}</p>
                 </section>
             </div>
         </div>
