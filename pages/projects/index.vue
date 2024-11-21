004773cf354e091d73e60e0e70d53c01e117bcbf
kouacc <maxrur57@gmail.com>
2024-11-22 00:32:18 +0100

    feat: add pagination for projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,6 +3,7 @@ import { onMounted } from "vue";
 import gsap from "gsap";
 
 useHead({
+  title: "Projets",
   bodyAttrs: {
     class: "gradient-bg dark:gradient-bg-dark",
   },
@@ -11,6 +12,20 @@ useHead({
 const { data } = await useFetch("/api/projects/fetchprojects");
 
 const latestProjects = data.value?.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()).slice(0, 3);
+
+const page = ref<number>(1);
+
+const shownProjects = computed(() => {
+  return data.value.slice((page.value - 1) * 8, page.value * 8);
+});
+
+function handlePagination(direction: 'prev' | 'next') {
+  if (direction === 'prev' && page.value > 1) {
+    page.value--;
+  } else if (direction === 'next' && data.value.length > page.value * 8) {
+    page.value++;
+}
+}
 
 onMounted(() => {
   gsap.from('.trigger-0', { opacity: 0, x: -50, duration: 1 });
@@ -30,20 +45,28 @@ onMounted(() => {
     </h1>
     
     <div class="grid grid-cols-3 gap-8">
-      <ProjectCard class="trigger-0 col-span-2 row-span-2 w-full" :href="`/projects/${latestProjects[0].id}`" :projectName="latestProjects[0].title" :projectDate="latestProjects[0].year" :projectImg="`/content/${latestProjects[0].id}/${latestProjects[0].cover}`"/>
-      <ProjectCard class="trigger-1 col-start-3" :href="`/projects/${latestProjects[1].id}`" :projectName="latestProjects[1].title" :projectDate="latestProjects[1].year" :projectImg="`/content/${latestProjects[0].id}/${latestProjects[0].cover}`"/>
-      <ProjectCard class="trigger-2 col-start-3" :href="`/projects/${latestProjects[2].id}`" :projectName="latestProjects[2].title" :projectDate="latestProjects[2].year" :projectImg="`/content/${latestProjects[0].id}/${latestProjects[0].cover}`"/>
+      <h2 class="col-span-full col-start-1 hidden md:block">Projets récents</h2>
+      <h2 class="col-span-full col-start-1 block md:hidden">Projet récent</h2>
+        <ProjectCard
+          v-for="(project, index) in latestProjects"
+          :key="project.id" 
+          :class="`trigger-${index} ${index === 0 ? 'col-span-full md:col-span-2 row-span-2 w-full' : 'col-start-3 hidden md:block'}`"
+          :href="`/projects/${project.id}`"
+          :projectName="project.title"
+          :projectDate="project.year"
+          :projectImg="`/content/${project.id}/${project.cover}`"
+        />
     </div>
     <div class="space-y-14">
       <section class="flex justify-between items-center">
         <h2>Tous mes projets</h2>
         <div v-if="data && data.length >= 8" class="flex gap-2">
-          <button class="pagination-btn"><Icon name="lucide:chevron-left" size="24" /></button>
-          <button class="pagination-btn"><Icon name="lucide:chevron-right" size="24" /></button>
+          <button class="pagination-btn" @click="handlePagination('prev')"><Icon name="lucide:chevron-left" size="24" /></button>
+          <button class="pagination-btn" @click="handlePagination('next')"><Icon name="lucide:chevron-right" size="24" /></button>
         </div>
       </section>
       <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        <ProjectCard class="trigger" v-for="projet in data" :key="projet.id" :href="`/projects/${projet.id}`" :projectName="projet.title" :projectDate="projet.year" :projectImg="`/content/${projet.id}/${projet.cover}`" />
+        <ProjectCard class="trigger" v-for="projet in shownProjects" :key="projet.id" :href="`/projects/${projet.id}`" :projectName="projet.title" :projectDate="projet.year" :projectImg="`/content/${projet.id}/${projet.cover}`" />
       </div>
     </div>
     
