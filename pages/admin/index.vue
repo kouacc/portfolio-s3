dc2b5f5ec33d76b380c308bc245bcc7f367a7ea3
kouacc <maxrur57@gmail.com>
2024-11-22 00:32:32 +0100

    feat: add projects imports

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-/* definePageMeta({
-  middleware: ['auth']
-}) */
+definePageMeta({
+  middleware: 'auth'
+})
 
 useHead({
   bodyAttrs: {
@@ -10,7 +10,7 @@ useHead({
   title: "Gestion - Maxence Lallemand",
 });
 
-interface JWT {
+export interface JWT {
   payload: {
     exp: number;
     iat: number;
@@ -24,15 +24,41 @@ interface JWT {
 }
 
 const cookie = useCookie<string>("token");
+const showProjectImportsWindow = ref<boolean>(false);
+const dropzone = ref<HTMLDivElement>();
+const isFileSelected = ref<boolean>(false)
+const file = ref<File>()
+
+function onDrop(event: Event) {
+    event.preventDefault();
+    const target = event.target as HTMLInputElement;
+    if (!target?.files || target.files.length === 0) {
+        return;
+    } else {
+      isFileSelected.value = true
+      file.value = target.files[0]
+      console.log(file.value)
+    }
+}
+
+async function importProjects() {
+  const request = await $fetch("/api/protected/importProject", {
+    method: "POST",
+    headers: {
+      Authorization: cookie.value,
+    },
+    body: file.value
+  })
+  if (request) {
+    return request.status 
+  } else {
+    return "Impossible d'importer les projets. Veuillez réessayer."
+  }
+}
+
 
 const { data, status } = await useFetch("/api/projects/fetchprojects");
-const token = await useFetch<JWT>("/api/protected/decode", {
-  onRequest({ request, options }) {
-    if (cookie.value) {
-      options.headers.set("Authorization", cookie.value);
-    }
-  },
-});
+const { payload } = await useJWT(cookie.value)
 
 async function exportData(table: "Projects" | "Contact") {
   const { data } = await useFetch(`/api/protected/export${table}`, {
@@ -77,7 +103,7 @@ async function deleteProject(id: number) {
 <template>
   <div class="container pt-32 pb-64 space-y-16">
     <div class="dot-grid dark:dot-grid-dark px-3 py-5 w-fit">
-      <h1>Bienvenue, {{ token.data.value?.payload.name }} !</h1>
+      <h1>Bienvenue, {{ payload.surname }} !</h1>
     </div>
     <div class="space-y-8">
       <section class="flex justify-between items-center">
@@ -97,7 +123,7 @@ async function deleteProject(id: number) {
         >
           <h3 class="font-geist text-[1.25rem]">{{ projet.title }}</h3>
           <div class="inline-flex items-center gap-6">
-            <span class="font-geist text-base"
+            <span class="hidden md:block font-geist text-base"
               >Ajouté le : {{ formatDate(projet.created_at) }}</span
             >
             <div class="inline-flex items-center gap-5">
@@ -130,8 +156,8 @@ async function deleteProject(id: number) {
           /></ActionButton>
         </li>
         <li>
-          <ActionButton variant="secondary" @click="exportData('Projects')"
-            ><span class="text-white">Exporter projets</span
+          <ActionButton variant="secondary" @click="showProjectImportsWindow = true"
+            ><span class="text-white">Import/export projets</span
             ><Icon name="lucide:file-symlink" size="24" class="text-white"
           /></ActionButton>
         </li>
@@ -151,6 +177,33 @@ async function deleteProject(id: number) {
           /></ActionButton>
         </li>
       </ul>
+      <div v-show="showProjectImportsWindow" class="bg-black/40 fixed top-0 left-0 w-screen h-screen !mt-0 flex items-center justify-center sm:grid">
+        <div class="sm:place-self-center rounded-xl primary-bg p-8 space-y-5 w-auto h-auto">
+          <section class="flex justify-between">
+        <h2>Import/export</h2>
+        <button class="flex items-center px-3 hover:bg-black/20 transition-all rounded-xl" @click="showProjectImportsWindow = false"><Icon name="lucide:x" size="24"/></button>
+          </section>
+          <div class="flex flex-col sm:flex-row divide-y sm:divide-x sm:divide-y-0 divide-secondary dark:divide-secondary_dark">
+        <div class="space-y-4 pb-4 sm:pr-4 sm:pb-0">
+          <h3 class="font-geistmono text-xl">Importer</h3>
+          <div ref="dropzone" class="rounded-lg border secondary-border px-10 py-20 flex flex-col gap-10 items-center" :class="{ 'pb-3' : isFileSelected}">
+            <input class="file:rounded-full file:px-3 file:py-2 file:border-none file:secondary-bg file:fill-text" type="file" accept="application/json" @change="(e) => onDrop(e)" />
+            <button v-show="isFileSelected" class="bg-green-500 px-4 py-2 rounded-xl" @click="importProjects()">Importer</button>
+            <p>{{ 'Projets importés !' }}</p>
+          </div>
+        </div>
+        <div class="space-y-4 pt-4 sm:pl-4 sm:pt-0">
+          <h3 class="font-geistmono text-xl">Exporter</h3>
+          <div class="flex justify-center items-center p-16">
+            <ActionButton variant="secondary" @click="exportData('Projects')">
+          <span class="text-white">Exporter</span>
+          <Icon name="lucide:rectangle-ellipsis" size="24" class="text-white"/>
+            </ActionButton>
+          </div>
+        </div>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
