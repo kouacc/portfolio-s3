4d22db06ad8e60c776b7006a8af0fd3fdf8bcf4e
kouacc <maxrur57@gmail.com>
2024-11-22 00:30:37 +0100

    chore: add discord webhook request when receiving contact form

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -5,10 +5,30 @@ import { contactTable } from "~/database/schema";
 export default defineEventHandler(async (event) => {
   const form = await readFormData(event);
 
-  const name = form.get("name") as string;
-  const surname = form.get("surname") as string;
+  const name = form.get("lastname") as string;
+  const surname = form.get("firstname") as string;
   const email = form.get("email") as string;
   const message = form.get("message") as string;
+
+  //envoi webhook
+  await $fetch(process.env.DISCORD_WEBHOOK as string, {
+    method: "POST",
+    body: {
+      username: "Portfolio",
+      embeds: [
+        {
+          title: "Nouveau contact",
+          type: "rich",
+          description: `Nom : ${name}\nPrénom : ${surname}\nEmail : ${email}\nMessage : ${message}`,
+          color: 0x00ff00,
+          footer: {
+            text: "Logs - Portfolio",
+          },
+          timestamp: new Date(),
+        },
+      ],
+    },
+  });
 
   //envoi db
   await db.insert(contactTable).values({
