2b497393f34a8376699b342ede68f6074727de2c
Maxence Lallemand <maxence.lallemand@edu.univ-fcomte.fr>
2024-11-23 13:08:22 +0100

    feat: add favicon and variants

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,18 @@ export default defineNuxtConfig({
     viewTransition: true
   }, */
 
+  app: {
+    head: {
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
+
   runtimeConfig: {
     JWT_SECRET: '',
     ADMIN_PASSWORD: ''
