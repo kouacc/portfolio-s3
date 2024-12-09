7a42cb826116257341ae7867d1251cd787aff66a
kouacc <maxrur57@gmail.com>
2024-12-09 15:49:49 +0100

    fix: add missing commas in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,12 +14,11 @@ export default defineNuxtConfig({
   sitemap: {
     exclude: ['/admin/**'],
     include: ['/'],
-    
-  }
+  },
 
   routeRules: {
     '/admin/**': { robots: false }
-  }
+  },
 
   /* experimental: {
     viewTransition: true
