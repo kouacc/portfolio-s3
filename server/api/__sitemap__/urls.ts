6bd1f6454c042b640016351c215debbf7f1bd106
kouacc <maxrur57@gmail.com>
2024-12-14 10:52:34 +0100

    fix: change to db query instead of route to get correct modified_at param

--- server/api/__sitemap__/urls.ts
+++ server/api/__sitemap__/urls.ts
@@ -2,11 +2,16 @@ import { defineSitemapEventHandler } from "#imports";
 import type { SitemapUrlInput } from "#sitemap/types";
 
 export default defineSitemapEventHandler(async () => {
-    const response = await $fetch('/api/projects/fetchprojects');
+    const response = await db.query.projectTable.findMany({
+        columns: {
+            id: true,
+            modified_at: true
+        }
+    })
 
     return response.map((project): SitemapUrlInput => ({
         loc: `/projects/${project.id}`,
-        lastmod: new Date(),
+        lastmod: new Date(project.modified_at).toISOString(),
         priority: 0.8,
         changefreq: 'monthly'
     }));
